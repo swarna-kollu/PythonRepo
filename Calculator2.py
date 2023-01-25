2e3b89e805f4118c1bbf267c20c59fbcc24842a4
swarna-kollu <120728662+swarna-kollu@users.noreply.github.com>
2023-01-25 10:40:36 +0100

    Update Calculator2.py

--- Calculator2.py
+++ Calculator2.py
@@ -24,16 +24,3 @@ else:
     print("Invalid input")
 
 
-"""select = int(input("Select operations form 1, 2, 3, 4 :"))
-num_1 = int(input("Enter first number: "))
-num_2 = int(input("Enter second number: "))
-if select == 1:
-    print(num_1, "+", num_2, "=",num_1+num_2)
-elif select == 2:
-    print(num_1, "-", num_2, "=",num_2-num_2)
-elif select == 3:
-    print(num_1, "*", num_2, "=",num_1*num_2)
-elif select == 4:
-    print(num_1, "/", num_2, "=",num_1/num_2)
-else:
-    print("Invalid input")"""
